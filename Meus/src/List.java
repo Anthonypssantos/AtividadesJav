7ae483b2e24421a8d410e1e45c7c4485d6a978cd
Anthonypssantos <104658705+Anthonypssantos@users.noreply.github.com>
2022-08-03 21:36:56 -0300

    Lista

--- Meus/src/List.java
+++ Meus/src/List.java
@@ -1,4 +1,50 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class List {
-
+	
+	//Chamadas das classes
+	Mercado m = new Mercado();
+	ExerciciosF excc = new ExerciciosF();
+	Escola esc = new Escola();
+	
+	//array tipo string 
+	ArrayList<String> Lista = new ArrayList<>();
+	
+	//Variaveis String
+	String nome=" ", rtipo =" ";
+	
+	//Estrutura de repetição
+	boolean c = true;
+	
+	//Metodo listas
+	public void Lis(String rtipo) {
+		
+		if(rtipo.equals("Mercado")) {
+			
+			
+			
+		}if(rtipo.equals("Exercicios fisicos")){
+		
+			
+			
+		}if(rtipo.equals("Escola")) {
+			
+			
+			
+		}if(rtipo.equals("Mercado")) {
+			
+			
+			
+		}if(rtipo.equals("Mercado")) {
+			
+			
+		}else {
+			
+			JOptionPane.showMessageDialog(null, "Oops! Algo digitado errado.\nPor favor digite de novo exatamente como o programa mostra");
+			return;
+		
+		}
+	}
 }
--- Meus/src/Main.java
+++ Meus/src/Main.java
@@ -1,9 +1,56 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		List l = new List();
+		boolean c = true;
+		String casos=" ", tipo=" ";
+		
+		do {
+			
+		casos = JOptionPane.showInputDialog("Insira 'entrar' para adicionar os produtos na lista\nInsira 'sair' para sair do programa");
+			
+			switch(casos) {
+			
+			case "entrar":{
+				
+				tipo = JOptionPane.showInputDialog("Coloque o tipo de lista que desejas entrar\n==============================\nTipos de listas\n\nA: Mercado\nB: Exercicios fisicos\nC: Escola\n\nCaso você queira voltar para o menu principal\ninsira 'Voltar'\nPara sair do programa insira 'Sair do programa'\n\n===LEMBRETE===\nDigite o nome da lista.\nEXEMPLO: Mercado ou etc...");
+				
+				if(tipo.equals("Voltar")) {
+					
+					return;
+					
+				}else if(tipo.equals("Sair do programa")) {
+					
+					c = false;
+					
+				}else {
+					
+					JOptionPane.showMessageDialog(null, "Você escolheu a lista tipo: "+ tipo);
+					l.Lis(tipo);
+					
+				}
+				
+				break;
+				}
+			
+			
+			default:{
+					
+					JOptionPane.showMessageDialog(null, "Obrigado, volte sempre!");
+					c = false;
+					
+				break;
+				}
+			
+			}
+			
+		}while(c == true);
+		
+		
 	}
 
 }
