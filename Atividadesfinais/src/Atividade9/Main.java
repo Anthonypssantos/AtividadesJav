ce5ecb47e09585f0f6ee4cd4845750891b20e8e4
Anthonypssantos <104658705+Anthonypssantos@users.noreply.github.com>
2022-08-17 19:11:20 -0300

    java4

--- Atividadesfinais/src/Atividade9/Main.java
+++ Atividadesfinais/src/Atividade9/Main.java
@@ -1,13 +1,44 @@
 package Atividade9;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		arraygordo arr = new arraygordo();
 		
+		boolean v = true;
 		
+		int casos=0;
 		
+		do {
+			
+			casos = Integer.parseInt(JOptionPane.showInputDialog("1 para add os elementos no conjunto\n2 para comparar\n3 para sair"));
+			
+			switch(casos) {
+			
+			case 1:{
+				arr.pegaString();
+				break;
+			}
+			
+			case 2:{
+				arr.compara();
+				break;
+			}
+			default:{
+				
+				v = false;
+				
+				break;
+				}
+			
+			}
+			
+		}while(v == true);
+	
 	}
 
 }
